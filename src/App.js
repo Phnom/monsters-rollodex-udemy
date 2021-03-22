927216d003a74301135727a0e290df0fbcf057b2
Phnome <soold88@hotmail.se>
2021-03-22 15:43:39 +0100

    appen

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react"
 import "./App.css"
 import { CardList } from "./components/card-list/card-list.component"
+import { SearchBox } from "./components/search-box/search-box.component"
 
 class App extends Component {
   constructor() {
@@ -15,19 +16,24 @@ class App extends Component {
       .then((response) => response.json())
       .then((users) => this.setState({ monsters: users }))
   }
+
+  handleChange = (e) => {
+    this.setState({ searchField: e.target.value })
+  }
+
   render() {
     const { monsters, searchField } = this.state
-    const filterMonsters = monsters.filter((monster) =>
+    const filteredMonsters = monsters.filter((monster) =>
       monster.name.toLowerCase().includes(searchField.toLowerCase())
     )
     return (
       <div className="App">
-        <input
-          type="search"
+        <h1>Monster Rolodex</h1>
+        <SearchBox
           placeholder="search monsters"
-          onChange={(e) => this.setState({ searchField: e.target.value })}
+          handleChange={this.handleChange}
         />
-        <CardList monsters={filterMonsters} />
+        <CardList monsters={filteredMonsters} />
       </div>
     )
   }
